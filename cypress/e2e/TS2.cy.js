6bfba0906d5d21cab597a4ea015794d980ddb051
Beata Silwanowicz <beatasilwanowicz@beatas-mbp.home>
2023-11-08 22:38:31 +0100

    new repo setup

---
+++ cypress/e2e/TS2.cy.js
@@ -0,0 +1,26 @@
+import HomePage from "../page_objects/homePage";
+import CartPage from "../page_objects/cartPage";
+import OrderPage from "../page_objects/orderPage";
+import { faker } from '@faker-js/faker';
+
+describe ('my 2nd scenario', () => {
+    const homePage = new HomePage();
+    const cartPage = new CartPage();
+    const orderPage = new OrderPage();
+
+    before(function (){
+        cy.fixture('products').as('productData')
+    })
+
+    it('should order a product from the shop', function(){
+        homePage.visitPage()
+        homePage.addProductToCart(this.productData.Polo)
+        homePage.clickGoToCartFromProductButton()
+        cartPage.checkIfAddedProductIsInCart(this.productData.Polo)
+        cartPage.clickGoToPayments()
+        orderPage.fillAllRequiredData()
+        orderPage.clickPlaceOrderButton()
+        orderPage.checkIfOrderWasPlacedSuccesfully()
+
+    })
+})    
